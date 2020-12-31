d530c741b6c7f033c1375001f078e6e548e1c5b0
Hamed Jabbari <jabbary.hamed@gmail.com>
2020-12-31 17:38:50 -0500

    Delete and read functionallity done

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const signUpTemplate = require('../models/SignupModels')
 const bcrypt = require('bcrypt');
 
+
+
+// Add a new user to DB
 router.post('/signup', async (req, res) => {
 
     const saltPassword = await bcrypt.genSalt(10)
@@ -15,13 +18,32 @@ router.post('/signup', async (req, res) => {
         email: req.body.email,
         password: securePassword
     })
-
-
-
+ 
     signedUpUser.save()
         .then(data => res.json(data))
         .catch(error => res.json(error))
 })
 
+
+// Get users frokm DB
+router.get('/grab', (req, res) => {
+
+    signUpTemplate.find().then(data => {
+        res.json(data)
+    })
+})
+
+
+// 
+router.delete('/delete', (req, res) => {
+    
+    signUpTemplate.deleteOne({ _id: req.body.id })
+    .then((data) => {
+
+       res.json(data);
+    })
+     
+        
+} )
 
 module.exports = router
--- src/App.js
+++ src/App.js
@@ -8,15 +8,25 @@ export default class App extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {
-
+        this.state = { 
             fullname: '',
             username: '',
             email: '',
             password: '',
-            isSubmitted: ''
+            isSubmitted: '',
+            users: []
         }
     }
+
+    componentDidMount(){
+        axios.get('http://localhost:4000/app/grab')
+        .then(response => {
+            this.setState({
+                users: response.data
+            })
+        })
+    }
+ 
 
     handleChange = (e) => {
 
@@ -37,19 +47,42 @@ export default class App extends Component {
 
         axios.post('http://localhost:4000/app/signup', newUser).then(response => {
             
-        console.log('object');
             this.setState({
-                isSubmitted: 'Form submitted!'
-            })
-        })
+                isSubmitted: 'Form submitted!',
+                fullname: '',
+                username: '',
+                email: '',
+                password: ''
+            }) 
 
+            setTimeout(() => {
+                this.setState({
+                    isSubmitted: ''
+                }) 
+            }, 4000);
+        })
+        window.location.reload();
+      
+    }
+
+
+    handleDelete = (userId) => {
+       axios.delete('http://localhost:4000/app/delete', {data: {id: userId}})
+       .then((data)=>{console.log(data)})
+    
+        window.location.reload();
     }
 
     render() {
+
+       const userJSX = this.state.users.map(user => <li className="list-group-item" key={user._id}>{user.fullname},  {user.username} <i className="fa fa-trash" onClick={()=>{this.handleDelete(user._id)}}></i> </li>)
+        
+      
         return (
+  
             <div>
                 <div className="container p-5 mt-5 bg-info">
-                    <h2 className="bg-danger">{this.state.isSubmitted}</h2>
+                    <h2 className="bg-warning">{this.state.isSubmitted}</h2>
                     <form onSubmit={this.handleSubmit} >
                         <div className="form-group">
                             <input type="text" className="form-control" placeholder="Fullname" name="fullname" value={this.state.fullname} onChange={this.handleChange} />
@@ -64,8 +97,19 @@ export default class App extends Component {
                             <input type="password" className="form-control" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} />
                         </div>
 
-                        <button className="btn btn-warning btn-block">Submit</button>
+                        <button className="btn bg-danger btn-block" 
+                         disabled={
+                            !this.state.fullname ||
+                            !this.state.username ||
+                            !this.state.email ||
+                            !this.state.password} >Submit</button>
                     </form>
+                 
+                     <h5 className="mt-5 text-secondary">List of data</h5>
+                    <ul className="list-group">
+                    
+                    {userJSX}
+                    </ul> 
                 </div>
 
             </div>
